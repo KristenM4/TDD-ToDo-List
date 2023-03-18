e0c473816c919c5e2eb391fb39fd2bf2d8fcf798
Kristen Martin <kristen4810@gmail.com>
2023-03-19 03:43:37 +0800

    add check_for_row_in_list_table helper function to FT

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Someone heard about our website, a to-do list maker, and wants to check it out
         self.browser.get('http://localhost:8000')
@@ -35,10 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Get car oil changed" as a list item
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1) #This is an explicit wait
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Get car oil changed", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Get car oil changed")
 
         # The option to add another item to the to-do list is there
         # User adds "Wash car" as the second item on the list
@@ -49,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page is updated and now shows 2 items in the to-do list
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Get car oil changed", [row.text for row in rows])
-        self.assertIn("2: Wash car", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Get car oil changed")
+        self.check_for_row_in_list_table("2: Wash car")
 
         # The site has a unique URL for this user and contains an explanation
         # to save the URL and revisit it to view the list again
