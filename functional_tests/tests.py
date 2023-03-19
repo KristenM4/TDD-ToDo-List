0b53cbc444748cc2eb0345545d63c766ea447b59
Kristen Martin <kristen4810@gmail.com>
2023-03-19 19:29:16 +0800

    create a retry loop in wait_for_row_in_list_table and remove explicit waits

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,10 +16,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Someone heard about our website, a to-do list maker, and wants to check it out
@@ -40,8 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         # After hitting enter, the page updates and says
         # "1: Get car oil changed" as a list item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1) #This is an explicit wait
-        self.check_for_row_in_list_table("1: Get car oil changed")
+        self.wait_for_row_in_list_table("1: Get car oil changed")
 
         # The option to add another item to the to-do list is there
         # User adds "Wash car" as the second item on the list
@@ -49,11 +59,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Wash car")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page is updated and now shows 2 items in the to-do list
-        self.check_for_row_in_list_table("1: Get car oil changed")
-        self.check_for_row_in_list_table("2: Wash car")
+        self.wait_for_row_in_list_table("1: Get car oil changed")
+        self.wait_for_row_in_list_table("2: Wash car")
 
         # The site has a unique URL for this user and contains an explanation
         # to save the URL and revisit it to view the list again
